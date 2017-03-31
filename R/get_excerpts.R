f58f6e10cbe5e554682625fe759b60af37249b8b
fvafrCU <fvafrcu@arcor.de>
2017-03-31 12:27:40 +0200

    Uncommented changes: backing up

--- R/get_excerpts.R
+++ R/get_excerpts.R
@@ -1,8 +1,12 @@
-get_excerpts <- function(force = FALSE) {
+get_excerpts <- function(force = FALSE, remove_dot_git = TRUE) {
     root <- rprojroot::is_r_package
     python_code <- file.path(root$find_file(), "inst", "excerpts")
     if (isTRUE(force)) unlink(python_code, recursive = TRUE)
     status <- git2r::clone("https://github.com/fvafrCU/excerpts/", 
                            python_code)
+    if (remove_dot_git) {
+        status <- capture.output(status)
+        unlink(file.path(python_code, ".git/"), recursive = TRUE, force = TRUE)
+    }
     return(status)
 }
