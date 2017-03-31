f0c474f6383ac9cdda4e24a25f1e3af1e7245d2e
fvafrCU <fvafrcu@arcor.de>
2017-03-31 23:06:44 +0200

    Uncommented changes: backing up

--- R/excerptr.R
+++ R/excerptr.R
@@ -1,20 +1,3 @@
-concatenate_python_codes <- function(codes) {
-    code <- NULL
-    for (code_file in codes) {
-        code <- c(code, readLines(code_file))
-    }
-    local_import_index <- grep("from \\. import", code)
-    local_imports <- trimws(unique(sapply(strsplit(code[local_import_index], 
-                                                   split = "import"), "[[", 2)))
-    code <- code[- local_import_index]
-    for (local_import in local_imports) {
-        code <- gsub(paste0(local_import, "\\."), "", code)
-    }
-    # get rid of imports from __future__ as I use them only to soothe pylint:
-    future_index <- grep("from __future__ import ", code)
-    code <- code[- future_index]
-    return(code)
-}
 #' root <- system.file(package = "excerptr")
 #' test_files <- file.path(root, "inst", "excerpts", "tests", "files")
 #' 
@@ -25,14 +8,7 @@ excerptr <- function(file_name, comment_character = "#", magic_character = "%",
              output_path = "", allow_pep8 = TRUE,
              prefix = "", postfix = "", run_pandoc = TRUE,
              compile_latex = FALSE, pandoc_formats = "tex") {
-    root <- system.file(package = "excerptr")
-    if (nchar(root) == 0)
-        root <- rprojroot::find_root(criterion = rprojroot::is_r_package)
-    python_code <- file.path(root, "inst", "excerpts")
-    python_dir <- file.path(python_code, "excerpts")
-    codes <- list.files(python_dir, pattern = "^[^_].*\\.py", full.names = TRUE)
-    code <- concatenate_python_codes(codes)
-    rPython::python.exec(code)
+    load_excerpts()
     status <- rPython::python.call("excerpts", file_name = file_name, 
                                    comment_character = comment_character, 
                                    magic_character = magic_character,
--- R/get_excerpts.R
+++ R/get_excerpts.R
@@ -1,6 +1,34 @@
+excerpts_path <- function(temporary = FALSE) {
+    base <- c("inst", "excerpts")
+    root <- tryCatch(rprojroot::find_root(rprojroot::is_r_package), 
+                     error = function(e) return(FALSE)
+                     )
+    if (root == FALSE) {
+        excerptr_excerpts <- system.file(base, package = "excerptr")
+        if (excerptr_excerpts == "" || isTRUE(temporary)) {
+            root <- tempdir()
+        } else {
+            root <- excerptr_excerpts
+        }
+    }
+    excerpts_path <- file.path(root, paste(base, collapse = .Platform$file.sep))
+    return(excerpts_path)
+}
+set_excerpts_path <- function(...) {
+    options(excerpts_path = excerpts_path(...))
+    return(invisible(NULL))
+}
+get_excerpts_path <- function() {
+    path <- getOption("excerpts_path")
+    if (is.null(path)) {
+        excerpts_path <- excerpts_path()
+    } else {
+        excerpts_path  <- path
+    }
+
+}
 get_excerpts <- function(force = FALSE, remove_dot_git = TRUE) {
-    root <- rprojroot::is_r_package
-    python_code <- file.path(root$find_file(), "inst", "excerpts")
+    python_code <- get_excerpts_path()
     if (isTRUE(force)) unlink(python_code, recursive = TRUE)
     status <- git2r::clone("https://github.com/fvafrCU/excerpts/", 
                            python_code)
@@ -8,5 +36,30 @@ get_excerpts <- function(force = FALSE, remove_dot_git = TRUE) {
         status <- capture.output(status)
         unlink(file.path(python_code, ".git/"), recursive = TRUE, force = TRUE)
     }
+    return(status)
+}
+concatenate_python_codes <- function(codes) {
+    code <- NULL
+    for (code_file in codes) {
+        code <- c(code, readLines(code_file))
+    }
+    local_import_index <- grep("from \\. import", code)
+    local_imports <- trimws(unique(sapply(strsplit(code[local_import_index], 
+                                                   split = "import"), "[[", 2)))
+    code <- code[- local_import_index]
+    for (local_import in local_imports) {
+        code <- gsub(paste0(local_import, "\\."), "", code)
+    }
+    # get rid of imports from __future__ as I use them only to soothe pylint:
+    future_index <- grep("from __future__ import ", code)
+    code <- code[- future_index]
+    return(code)
+}
+load_excerpts <- function(...) {
+    python_directory <- get_excerpts_path(...)
+    python_codes <- file.path(python_directory, "excerpts")
+    codes <- list.files(python_codes, pattern = "^[^_].*\\.py", full.names = TRUE)
+    code <- concatenate_python_codes(codes)
+    status <- rPython::python.exec(code)
     return(status)
 }
