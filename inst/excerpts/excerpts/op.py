5e7019c123e03d69193339784a7de1513883a73a
fvafrCU <fvafrcu@arcor.de>
2017-06-22 10:12:03 +0200

    revert op.py

--- inst/excerpts/excerpts/op.py
+++ inst/excerpts/excerpts/op.py
@@ -39,15 +39,18 @@ def pandoc(file_name, compile_latex=False, formats="tex"):
 
     Kwargs:
         file_name: The file on which to run pandoc.
-        formats: The pandoc output formats to be used. A comma separated
-        string, "html,tex" for example.
+        formats: The pandoc output formats to be used.
+                 A comma separated string ("html,tex" for example) a tuple or a
+                 list giving the formats.
         compile_latex: Compile the LaTeX file?
     Returns:
         0 if parsing was successful, 1 otherwise.
     """
     status = 1
     if is_tool("pandoc"):
-        for form in formats.split(","):
+        if isinstance(formats, str):
+            formats = formats.split(",")
+        for form in formats:
             subprocess.check_call(["pandoc", "-sN", file_name, "-o",
                                    main.modify_path(file_name=file_name,
                                                     extension=form)])
